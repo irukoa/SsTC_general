26b096c4c7f8ec3780a01583bc44b98cf5ca8628
Iruko <irukoa@live.com>
2023-08-10 18:24:45 +0200

    fix: style.

--- calculators_general.F90
+++ calculators_general.F90
@@ -19,14 +19,15 @@ contains
   !====BANDS CALCULATOR AND CONSTRUCTOR====!
   function SsTC_bands(k_data, system, k, error) result(u)
     class(SsTC_local_k_data), intent(in) :: k_data
-    type(SsTC_sys), intent(in) :: system
-    real(kind=dp), intent(in) :: k(3)
-    logical, intent(inout) :: error
+    type(SsTC_sys), intent(in)           :: system
+    real(kind=dp), intent(in)            :: k(3)
+    logical, intent(inout)               :: error
 
-    complex(kind=dp)                :: u(k_data%integer_indices(1))
+    complex(kind=dp) :: u(k_data%integer_indices(1))
 
     complex(kind=dp) :: hamiltonian(system%num_bands, system%num_bands), &
                         rot(system%num_bands, system%num_bands)
+
     real(kind=dp) :: eig(system%num_bands)
 
     hamiltonian = SsTC_wannier_hamiltonian(system, k)
@@ -40,10 +41,10 @@ contains
   !==========DEFAULT BANDS KPATH TASK==========!
   subroutine SsTC_bands_kpath_task_constructor(task, system, Nvec, vec_coord, nkpts)
 
-    type(SsTC_sys), intent(in)  :: system
-    integer, intent(in) :: Nvec
-    real(kind=dp), intent(in) :: vec_coord(Nvec, 3)
-    integer, intent(in) :: nkpts(Nvec - 1)
+    type(SsTC_sys), intent(in) :: system
+    integer, intent(in)        :: Nvec
+    real(kind=dp), intent(in)  :: vec_coord(Nvec, 3)
+    integer, intent(in)        :: nkpts(Nvec - 1)
 
     type(SsTC_kpath_task), intent(out) :: task
 
